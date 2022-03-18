ed1185a47b331c2114ae57b84fc19157ef476397
Anastasia Sikidina <anastasia.sikidina@yandex.ru>
2022-03-18 10:09:26 +0300

    Delete routes directory

--- routes/movies.js
+++
@@ -1,9 +0,0 @@
-const moviesRouter = require('express').Router();
-const { getMovies, createMovie, deleteMovie } = require('../controllers/movies');
-const { validateMovieInfo, validateMovieId } = require('../middlewares/validations');
-
-moviesRouter.get('/', getMovies);
-moviesRouter.post('/', validateMovieInfo, createMovie);
-moviesRouter.delete('/:movieId', validateMovieId, deleteMovie);
-
-module.exports = moviesRouter;
